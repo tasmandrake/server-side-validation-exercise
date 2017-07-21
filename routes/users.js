5f84967bd061ad116ff30bf5095629cb5fe05496
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-07-21 10:16:06 -0600

    Added password

--- routes/users.js
+++ routes/users.js
@@ -22,21 +22,21 @@ router.post('/' , (req, res, next) => {
   const username = req.body.users.username;
   const email = req.body.users.email;
   const phone = req.body.users.phone;
+  const password = req.body.password;
 
   if (!firstname) {
     return res.send('Firstname is required')
   } else if (!lastname) {
     return res.send('Lastname is required')
-  } else if (!username && username.length < 6 && username.includes(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g) && Number(username[0])) {
-    return res.send('Username is required and must be longer than 6 characters and must start with a letter')
-  } else if (!email && email.includes('@')) {
+  } else if (!username || username.length < 6 || username.match(/[.,\/#!$%\^&\*;:{}=\-_`~()]/) || Number(username[0])) {
+    return res.send('Username is required and must be longer than 6 characters and must start with a letter and contain no punctuation')
+  } else if (!email || !email.includes('@')) {
     return res.send('A valid email is required')
-  } else if (!phone && String(phone).length < 10 && String(phone).length > 10) {
+  } else if (!phone || String(phone).length < 10 || String(phone).length > 10) {
     return res.send('A valid 110 digit phone number is required')
+  } else if (!password || password.length < 8 || !password.match(/[!?\/.,')]/) || !password.match(/[0-9]/)) {
+    return res.send('Password is required and must be more than 8 characters, include a number and a special character')
   }
-  // else if (!password) {
-  //   return res.send('Firstname is required')
-  // }
   knex('users')
     .insert({
       firstname,
